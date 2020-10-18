8f0bb25b711a8bdd8cd9acce891a446ec230799e
Eric Brophy <eric@ericbrophy.com>
2020-10-17 23:48:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gh-action-webhook
+
+This action calls an api endpoint with a "GET" command
+
+## Inputs
+
+### `url`
+
+**Required** The url to call. Default `"https://www.google.com"`
+
+## Outputs
+
+## Example usage
+
+uses: ebrophy44/gh-action-webhook@v1.1
+with:
+  url: 'https://www.google.com'
+
---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const request = require('request')
+  const url = core.getInput('url')
+  request(url, 
+    function (error, response, body) {
+      console.error('error:', error); // Print the error if one occurred
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    }
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
+
